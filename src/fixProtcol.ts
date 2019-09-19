a226950f43c0384df5d65ec0a60ab9d15d2f6816
Gary Hughes <garyedwardhughes@gmail.com>
2019-09-19 18:58:50 +1000

    Basic message detection.

---
+++ src/fixProtcol.ts
@@ -0,0 +1,14 @@
+
+export const fixMessagePrefix = "8=FIX.";
+
+export class Message {
+
+}
+
+export function parseMessage(text:string) {
+    return new Message();
+}
+
+export function prettyPrintMessage(message:Message) {
+    return "PRETTY";
+}
