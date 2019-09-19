c198d3d2f1fd42bacb02934cfed04dff45dc1a6e
Gary Hughes <garyedwardhughes@gmail.com>
2019-09-19 22:12:23 +1000

    Basic FIX parser and initial format command.

--- src/extension.ts
+++ src/extension.ts
@@ -3,24 +3,36 @@ import { fixMessagePrefix, parseMessage, prettyPrintMessage } from './fixProtcol
 
 export function activate(context: vscode.ExtensionContext) {
 	vscode.commands.registerCommand('extension.format', () => {
+	
 		const {activeTextEditor} = vscode.window;
+	
 		if (activeTextEditor) {
+	
 			const {document} = activeTextEditor;
+			//var edits : vscode.WorkspaceEdit[] = []
+	
 			for (var index = 0; index < document.lineCount; ++index) {
+	
 				const line = document.lineAt(index);
 				const fixMessageIndex = line.text.indexOf(fixMessagePrefix); 
+	
 				if (fixMessageIndex < 0) {
 					continue;
 				}
+	
 				const message = parseMessage(line.text.substr(fixMessageIndex));	
+	
 				if (!message) {
 					continue;
 				}
+	
 				const pretty = prettyPrintMessage(message);
 				const edit = new vscode.WorkspaceEdit();
 				edit.replace(document.uri, line.range, pretty);
-				return vscode.workspace.applyEdit(edit);
+				vscode.workspace.applyEdit(edit)
+				//edits.push(edit)
 			}
+			//return edits
 		}
     });
 }
--- src/fixProtcol.ts
+++ src/fixProtcol.ts
@@ -1,14 +1,86 @@
 
-export const fixMessagePrefix = "8=FIX.";
+export const fixMessagePrefix = "8=FIX."
+export const fieldDelimiter = '\x01'
+export const fieldValueSeparator = '='
+export const checkSumTag = 10
+
+export class Field {
+
+    tag : number;
+    value : string;
+    
+    constructor(tag: number, value: string) {
+        this.tag = tag;
+        this.value = value;
+    }
+}
 
 export class Message {
+
+    fields : Field[]
+
+    constructor(fields : Field[]) {
+        this.fields = fields;
+    }
 
 }
 
 export function parseMessage(text:string) {
-    return new Message();
+
+    var fields : Field[] = [];
+    
+    let length = text.length;
+
+    for (var index = 0; index < length; ++index) {
+
+        var tag : string = ""
+        var value : string = ""
+
+        for (; index < length; ++index) {
+            let token = text[index];
+            if (token == fieldValueSeparator || token == fieldDelimiter) {
+                ++index;
+                break;
+            }
+            tag += token;
+        }
+
+        for (; index < length; ++index) {
+            let token = text[index];
+            if (token == fieldDelimiter) {
+                ++index;
+                break;
+            }
+            value += token;
+        }
+
+        let intTag = parseInt(tag);
+
+        if (intTag == NaN) {
+            continue;
+        }
+
+        let field = new Field(intTag, value);
+
+        fields.push(field);
+
+        if (field.tag == checkSumTag) {
+            break;
+        }
+    }
+
+    var message = new Message(fields);
+
+    return message;
 }
 
 export function prettyPrintMessage(message:Message) {
-    return "PRETTY";
+    var buffer : string = ""
+    var field : any
+    for (var index = 0; index < message.fields.length; ++index) {
+        let field = message.fields[index]
+        buffer += `(${field.tag}) - ${field.value}\n` 
+    }
+    buffer += "\n"
+    return buffer;
 }
