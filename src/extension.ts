cd106208611c33a06cfee93ef206748f115ce69e
Gary Hughes <garyedwardhughes@gmail.com>
2019-09-26 18:37:24 +1000

    Validate the repository when the format command is run.

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,13 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
+import * as fs from 'fs';
 import * as FIX from './fixRepository';
 import { fixMessagePrefix, parseMessage, prettyPrintMessage } from './fixProtcol';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	const configuration = vscode.workspace.getConfiguration();
-	var repositoryPath = configuration.get('fixmaster.repositoryPath') as string;
-	if (!repositoryPath) {
-		repositoryPath = "./repository";
-	}
-	if (!path.isAbsolute(repositoryPath)) {
-		repositoryPath = path.join(context.extensionPath, repositoryPath);
-	}
-	// TODO - check path and raise error
-	const repository = new FIX.Repository(repositoryPath);
-
 	vscode.commands.registerCommand('extension.format', () => {
-	
+
 		const {activeTextEditor} = vscode.window;
 		
 		if (!activeTextEditor) {
@@ -25,17 +15,33 @@ export function activate(context: vscode.ExtensionContext) {
 			return;
 		}
 
+		const configuration = vscode.workspace.getConfiguration();
+
+		var repositoryPath = configuration.get('fixmaster.repositoryPath') as string;
+		
+		if (!repositoryPath) {
+			repositoryPath = "./repository";
+		}
+		
+		if (!path.isAbsolute(repositoryPath)) {
+			repositoryPath = path.join(context.extensionPath, repositoryPath);
+		}
+		
+		if (!fs.existsSync(repositoryPath)) {
+			vscode.window.showErrorMessage("The repository path '" + repositoryPath + "' cannot be found.");
+			return;
+		}
+	
+		const repository = new FIX.Repository(repositoryPath);
+
 		const {document} = activeTextEditor;
 
 		const edit = new vscode.WorkspaceEdit();
-
-		const configuration = vscode.workspace.getConfiguration();
 
 		for (var index = 0; index < document.lineCount; ++index) {
 
 			const line = document.lineAt(index);
 			
-			// TODO - support multiple configurable prefixes 
 			const fixMessageIndex = line.text.indexOf(fixMessagePrefix); 
 
 			if (fixMessageIndex < 0) {
