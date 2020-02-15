df0b45342dc51d89e264c213edc31b1cf7317429
Gary Hughes <garyedwardhughes@gmail.com>
2020-02-15 17:28:07 +1100

    No hardcoded report fields, change the defaults.

--- src/orderReport.ts
+++ src/orderReport.ts
@@ -6,11 +6,6 @@ import { MessageField } from './definitions';
 export class OrderReport
 {
     public constructor(readonly repository: Repository, readonly orderBook: OrderBook, readonly fields: MessageField[]) {
-        this._report.addColumn("BeginString");
-        this._report.addColumn("SenderCompID");
-        this._report.addColumn("TargetCompID");
-        this._report.addColumn("ClOrdID");
-        this._report.addColumn("OrigClOrdID");
         this._fields = fields;
         for (let definition of this._fields) {
             let justification = ColumnJustification.Left;
@@ -26,16 +21,8 @@ export class OrderReport
         this._report.clear();
 
         for (let order of this.orderBook.orders.values()) {
-            
-            var values = new Array(5 + this._fields.length).fill(null);
-            
-            values[0] = order.beginString; 
-            values[1] = order.senderCompId;
-            values[2] = order.targetCompId;
-            values[3] = order.clOrdId;
-            values[4] = order.origClOrdId;
-            
-            var index = 5;
+            var values = new Array(this._fields.length).fill(null);
+            var index = 0;
             this._fields.forEach(definition => {
                 let field = order.fields[definition.field.tag];
                 if (field) {
